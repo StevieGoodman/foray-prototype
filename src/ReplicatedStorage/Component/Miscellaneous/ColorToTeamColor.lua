bc669982a92358c53b9f0df1faa87e7b4e5ed7a9
Stevie Goodman <mail@steviegoodman.com>
2024-12-07 13:26:45 +0000

    Added `ColorToTeamColor` component

---
+++ src/ReplicatedStorage/Component/Miscellaneous/ColorToTeamColor.lua
@@ -0,0 +1,33 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Component = require(ReplicatedStorage.Packages.Component)
+local Trove = require(ReplicatedStorage.Packages.Trove)
+
+local TeamComponent = require(ReplicatedStorage.Component.Team)
+
+local ColorToTeamColor = Component.new {
+    Tag = "ColorToTeamColor",
+    Ancestors = { game },
+}
+
+function ColorToTeamColor:Construct()
+    self._trove = Trove.new()
+end
+
+function ColorToTeamColor:SteppedUpdate()
+    local team = Players.LocalPlayer.Team
+    if team == nil then return end
+    team = TeamComponent:FromInstance(team)
+    if self.Instance:IsA("Frame") then
+        self.Instance.BackgroundColor3 = team.Color
+    else
+        warn(`Cannot color {self.Instance.ClassName} to a team color! Is the {self.Instance.ClassName} class missing an implementation?`)
+    end
+end
+
+function ColorToTeamColor:Stop()
+    self._trove:Clean()
+end
+
+return ColorToTeamColor
